da546e4686a6e2b8993f8c8bddefb85cb56bd59d
pimvanyperen-netizen <pim.van.yperen@gmail.com>
2025-11-03 09:15:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.stop()
 
 # convert the snowpark dataframe to a panda dataframe so we can use the LOC function
-pd_df = dataframe.to_pandas()
+pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
